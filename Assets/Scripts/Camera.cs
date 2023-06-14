44e47bcd1a17a404fa913151d1288c8c1255980e
vxt2001 <vxntng@gmail.com>
2023-06-15 00:31:58 +0700

    camera

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
+    public GameObject floor;
+    public GameObject floorpre;
+    public GameObject trannha;
+    public GameObject trannhapre;
+    public GameObject contho;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,17 @@ public class Camera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (contho.transform.position.x> floor.transform.position.x)
+        {
+            var tempCeiling = trannhapre;
+            var tempFloor = floorpre;
+            floorpre = floor;
+            trannhapre = trannha;
+            tempCeiling.transform.position += new Vector3(80, 0, 0);
+            tempFloor.transform.position += new Vector3(80, 0, 0);
+            trannha= tempCeiling;
+            floor=tempFloor;
+
+        }
     }
 }
