2209a6ee4d6e0ecb1c2bed4301e386358fb00e1e
vxt2001 <vxntng@gmail.com>
2023-07-04 19:00:53 +0700

    a

--- Assets/Scripts/menu/Menu.cs
+++ Assets/Scripts/menu/Menu.cs
@@ -17,18 +17,76 @@ public class Menu : MonoBehaviour
             Pause();
         }
     }
+
+    //private void Pause()
+    //{
+    //    Time.timeScale = 0f; // T?m d?ng game
+    //    isPaused = true;
+    //    AudioListener.pause = true; // T?m d?ng âm thanh
+
+    //    // T?m d?ng hi?n th? mesh (n?u có)
+    //    Renderer[] renderers = FindObjectsOfType<Renderer>();
+    //    foreach (Renderer renderer in renderers)
+    //    {
+    //        renderer.enabled = false;
+    //    }
+    //    SceneManager.LoadScene(2);
+    //}
+
+    //private void ResumeGame()
+    //{
+    //    //Time.timeScale = 1f; // Ti?p t?c game
+    //    //isPaused = false;
+    //    //AudioListener.pause = false; // Ti?p t?c phát âm thanh
+
+    //    //// Hi?n th? l?i mesh (n?u có)
+    //    //Renderer[] renderers = FindObjectsOfType<Renderer>();
+    //    //foreach (Renderer renderer in renderers)
+    //    //{
+    //    //    renderer.enabled = true;
+    //    //}
+    //    //SceneManager.LoadScene(1);
+
+    //    Time.timeScale = 1f; // Ti?p t?c game
+    //    AudioListener.pause = false; // Ti?p t?c phát âm thanh
+
+    //    // Hi?n th? l?i mesh (n?u có)
+    //    Renderer[] renderers = FindObjectsOfType<Renderer>();
+    //    foreach (Renderer renderer in renderers)
+    //    {
+    //        renderer.enabled = true;
+    //    }
+
+    //    SceneManager.LoadScene(1);
+    //}
     private void Pause()
     {
         Time.timeScale = 0f; // T?m d?ng game
         isPaused = true;
-        // Hi?n th? giao di?n t?m d?ng game (n?u c?n)
+        AudioListener.pause = true; // T?m d?ng âm thanh
+
+        // T?m d?ng hi?n th? mesh (n?u có)
+        Renderer[] renderers = FindObjectsOfType<Renderer>();
+        foreach (Renderer renderer in renderers)
+        {
+            renderer.enabled = false;
+        }
+        SceneManager.LoadScene(2); // Chuy?n ??n Scene Pause
     }
 
     private void ResumeGame()
     {
         Time.timeScale = 1f; // Ti?p t?c game
-        isPaused = false;
-        // ?n giao di?n t?m d?ng game (n?u c?n)
+        AudioListener.pause = false; // Ti?p t?c phát âm thanh
+
+        // Hi?n th? l?i mesh (n?u có)
+        Renderer[] renderers = FindObjectsOfType<Renderer>();
+        foreach (Renderer renderer in renderers)
+        {
+            renderer.enabled = true;
+        }
+
+        SceneManager.LoadScene(1); // Chuy?n ??n Scene Game
     }
     public void ChoiMoi()
     {
--- Assets/Scripts/nhanvatbay.cs
+++ Assets/Scripts/nhanvatbay.cs
@@ -1,110 +1,4 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
 
-//public class nhanvatbay : MonoBehaviour
-//{
-//    // Start is called before the first frame update
-//    public float jumpForce;
-
-//    public Rigidbody2D rb;
-//    public float speed;
-//    public Animator contho;
-//    public bool isDead = false;
-
-//    public Transform groundCheckTransform;
-//    private bool isGround;
-//    public LayerMask groundCheckLayerMask;
-
-//    private Collider2D laserCollider;
-//    private Collider2D groundCollider;
-//    private Collider2D raserCollider;
-
-//    private Cameradich cameraScript; // Tham chi?u t?i script "Cameradich"
-
-//    public GameObject rocketObject;
-
-//    public AudioSource footstepsAudio;
-//    public AudioSource jetpackAudio;
-
-
-//    void Start()
-//    {
-//        rb = GetComponent<Rigidbody2D>();
-//        contho = GetComponent<Animator>();
-//        HideGroundCheck();
-
-//        cameraScript = FindObjectOfType<Cameradich>();
-
-//        rocketObject = GameObject.Find("Square");
-//    }
-
-//    void Update()
-//    {
-//        bool jetpackActive = Input.GetButton("Fire1");
-//        jetpackActive = jetpackActive && !isDead;
-
-//        //if (jetpackActive)
-//        //{
-//        //    playerRigidbody.AddForce(new Vector2(0, jetpackForce));
-//        //}
-
-//        if (!isDead)
-//        {
-//            if (Input.GetKey(KeyCode.Space))
-//            {
-//                rb.AddForce(Vector2.up * jumpForce);
-//            }
-//        }
-//        else
-//        {
-//           // rb.velocity = Vector2.zero;
-//        }
-
-//        UpdateGroundedStatus();
-//        AdjustFootstepsAndJetpackSound(jetpackActive);
-//    }
-
-//    void UpdateGroundedStatus()
-//    {
-//        isGround = Physics2D.OverlapCircle(groundCheckTransform.position, 0.1f, groundCheckLayerMask);
-//        contho.SetBool("isGround", !isGround);
-//    }
-
-//    void OnTriggerEnter2D(Collider2D collider)
-//    {
-//        HitByLaser(collider);
-//    }
-
-//    void HideGroundCheck()
-//    {
-//        groundCheckTransform.gameObject.SetActive(false);
-//    }
-
-//    void HitByLaser(Collider2D laser)
-//    {
-//        isDead = true;
-//        contho.SetBool("isDead", true);
-//        cameraScript.isCharacterDead = true;
-
-//        rocketObject.SetActive(false);
-//    }
-
-//    void AdjustFootstepsAndJetpackSound(bool jetpackActive)
-//    {
-//        footstepsAudio.enabled = isDead==false && isGround;
-//        jetpackAudio.enabled = isDead==false && !isGround;
-
-//        if (jetpackActive)
-//        {
-//            jetpackAudio.volume = 1.0f;
-//        }
-//        else
-//        {
-//            jetpackAudio.volume = 0.5f;
-//        }
-//    }
-//}
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -127,7 +21,7 @@ public class nhanvatbay : MonoBehaviour
 
     public AudioSource footstepsAudio;
     public AudioSource jetpackAudio;
-
+    public AudioSource dieAudio;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -181,9 +75,11 @@ public class nhanvatbay : MonoBehaviour
 
     void HitByLaser(Collider2D laser)
     {
+      //  dieAudio.enabled = isDead == true;
         isDead = true;
         contho.SetBool("isDead", true);
         cameraScript.isCharacterDead = true;
+        dieAudio.Play(); // Phát âm thanh dieAudio
 
         rocketObject.SetActive(false);
     }
@@ -192,15 +88,17 @@ public class nhanvatbay : MonoBehaviour
     {
         footstepsAudio.enabled = isDead == false && isGround;
         jetpackAudio.enabled = isDead == false && !isGround;
-
+      
         if (jetpackActive)
         {
             jetpackAudio.volume = 1.0f;
         }
         else
         {
-            jetpackAudio.volume = 0.5f;
+            footstepsAudio.volume = 0.5f;
         }
+       
+
     }
 
     void UpdateFireObject()
