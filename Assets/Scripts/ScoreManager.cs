c9c64008fcba68e0dec616824d38f312ea4382f8
vxt2001 <vxntng@gmail.com>
2023-07-06 03:48:50 +0700

    m

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -5,28 +5,31 @@ using UnityEngine;
 public class ScoreManager : MonoBehaviour
 {
 
-    //private ScoreUI scoreUI;
+    private ScoreUI scoreUI;
 
-    //// Update is called once per frame
-    //public float distanceTraveled;
-    //public int pointsPerMeter = 100;
+    public float distanceTraveled;
+    public int pointsPerMeter = 100;
 
-    //private int score;
-    //void Start()
-    //{
-    ////    scoreUI = FindObjectOfType<ScoreUI>();
-    //}
-    //private void Update()
-    //{
-    //    // C?p nh?t quãng ???ng ?i ???c trong m?i khung hình
-    //    distanceTraveled += Time.deltaTime;
+    private int score;
 
-    //    // Tính ?i?m d?a trên quãng ???ng ?i ???c
-    //    score = Mathf.RoundToInt(distanceTraveled * pointsPerMeter);
-    //}
+    void Start()
+    {
+        scoreUI = FindObjectOfType<ScoreUI>();
+        score = 0;
+    }
 
-    //private void UpdateScoreUI()
-    //{
-    //    scoreUI.UpdateScore(score);
-    //}
+    void Update()
+    {
+        // C?p nh?t quãng ???ng ?i ???c trong m?i khung hình
+        distanceTraveled += Time.deltaTime;
+
+        // Tính ?i?m d?a trên quãng ???ng ?i ???c
+        score = Mathf.RoundToInt(distanceTraveled * pointsPerMeter);
+
+        // C?p nh?t UI c?a ?i?m s?
+        if (scoreUI != null)
+        {
+            scoreUI.UpdateScore(score);
+        }
+    }
 }
--- Assets/Scripts/ScoreUI.cs
+++ Assets/Scripts/ScoreUI.cs
@@ -6,16 +6,10 @@ using UnityEngine.UI;
 
 public class ScoreUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    //private Text scoreText;
+    public Text scoreText;
 
-    //private void Awake()
-    //{
-    //    scoreText = GetComponent<Text>();
-    //}
-
-    //public void UpdateScore(int score)
-    //{
-    //    scoreText.text = "Score: " + score.ToString();
-    //}
+    public void UpdateScore(int score)
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
 }
