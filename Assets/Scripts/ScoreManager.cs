e8b779b41f58e70acf88c9cf69cde6c2a839cf6f
vxt2001 <vxntng@gmail.com>
2023-07-06 18:15:48 +0700

    a

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,35 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
-
-    private ScoreUI scoreUI;
-
+    public TMP_Text text;
     public float distanceTraveled;
     public int pointsPerMeter = 100;
-
+    
     private int score;
+    private nhanvatbay player;
 
     void Start()
     {
-        scoreUI = FindObjectOfType<ScoreUI>();
         score = 0;
+        player = FindObjectOfType<nhanvatbay>();
     }
 
     void Update()
     {
-        // C?p nh?t quãng ???ng ?i ???c trong m?i khung hình
-        distanceTraveled += Time.deltaTime;
+        //// C?p nh?t quãng ???ng ?i ???c trong m?i khung hình
+        //distanceTraveled += Time.deltaTime;
 
-        // Tính ?i?m d?a trên quãng ???ng ?i ???c
-        score = Mathf.RoundToInt(distanceTraveled * pointsPerMeter);
+        //// Tính ?i?m d?a trên quãng ???ng ?i ???c
+        //score = Mathf.RoundToInt(distanceTraveled * pointsPerMeter);
+
+        //// C?p nh?t UI c?a ?i?m s?
+        //text.text = score.ToString() + "m";
+        if (!player.isDead)
+        {
+            // C?p nh?t quãng ???ng ?i ???c trong m?i khung hình
+            distanceTraveled += Time.deltaTime;
+
+            // Tính ?i?m d?a trên quãng ???ng ?i ???c
+            score = Mathf.RoundToInt(distanceTraveled * pointsPerMeter);
+        }
 
         // C?p nh?t UI c?a ?i?m s?
-        if (scoreUI != null)
-        {
-            scoreUI.UpdateScore(score);
-        }
+        text.text = score.ToString() + "m";
     }
 }
+
+
+
+
+
